176ded5227d091ba3854685ae90da47b97165fca
danielfetz <118893474+danielfetz@users.noreply.github.com>
2023-01-23 21:19:35 +0100

    Create index.tsx

---
+++ src/components/chatactivity/index.tsx
@@ -0,0 +1,24 @@
+import { Box } from '@mui/material'
+import type { ReactElement, ReactNode } from 'react'
+
+import NavTabs from '@/components/common/NavTabs'
+import PageHeader from '@/components/common/PageHeader'
+import { chatactivityNavItems } from '@/components/sidebar/SidebarNavigation/config'
+
+const AssetsHeader = ({ children }: { children?: ReactNode }): ReactElement => {
+  return (
+    <PageHeader
+      title="Assets"
+      action={
+        <>
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <NavTabs tabs={chatactivityNavItems} />
+            {children}
+          </Box>
+        </>
+      }
+    />
+  )
+}
+
+export default AssetsHeader
