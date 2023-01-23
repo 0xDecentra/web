9c2cdb5caab279c8684fcd4181a7038f1188865e
danielfetz <118893474+danielfetz@users.noreply.github.com>
2023-01-23 20:51:46 +0100

    Update config.tsx

--- src/components/sidebar/SidebarNavigation/config.tsx
+++ src/components/sidebar/SidebarNavigation/config.tsx
@@ -80,7 +80,7 @@ export const balancesNavItems = [
 export const chatactivityNavItems = [
   {
     label: 'Chat',
-    href: AppRoutes.home.chat,
+    href: AppRoutes.home,
   },
   {
     label: 'Activity',
