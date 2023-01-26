3f28ca64e640b32ee0ae96fb2b55409ad7a21bfc
danielfetz <118893474+danielfetz@users.noreply.github.com>
2023-01-26 22:52:40 +0100

    Update index.tsx

--- src/components/common/PageLayout/index.tsx
+++ src/components/common/PageLayout/index.tsx
@@ -22,8 +22,6 @@ const PageLayout = ({ pathname, children }: { pathname: string; children: ReactE
         <PsaBanner />
         <Header onMenuToggle={setSidebarOpen} />
       </header>
-      
-            <SideDrawer isOpen={isSidebarOpen} onToggle={setSidebarOpen} className={css.sidedrawermobile} />
 
 <Grid container spacing={3} p={3} pb={0} flex={1} className={css.gridsidecontainer}>
    <Grid item xs={12} md={4} lg={3.5} maxWidth={{ md: 400 }} className={css.sidebar}>
