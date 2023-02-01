63945655975eedf06c0d9e874e43d23e0767876d
danielfetz <118893474+danielfetz@users.noreply.github.com>
2023-02-01 19:22:20 +0100

    Update index.tsx

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,11 +1,11 @@
 import type { ReactElement } from 'react'
 import { Grid } from '@mui/material'
-import PendingTxsList from '@/components/dashboard/PendingTxs/PendingTxsList'
 import Overview from '@/components/dashboard/Overview/Overview'
 import { FeaturedApps } from '@/components/dashboard/FeaturedApps/FeaturedApps'
 import CreationDialog from '@/components/dashboard/CreationDialog'
 import { useRouter } from 'next/router'
 import Owner from '@/components/dashboard/HomeSidebar/ownerlist'
+import Home from '@/pages/chat'
 
 const Dashboard = (): ReactElement => {
   const router = useRouter()
@@ -19,7 +19,7 @@ const Dashboard = (): ReactElement => {
         </Grid>
 
         <Grid item xs={12} md={12} lg={8}>
-          <PendingTxsList size={4} />
+          <Home />
         </Grid>
 
         <Grid item xs={4}>
