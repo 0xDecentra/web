fc15295cbdea5bd226e0c335ccfe1231e3829b0c
nitegeist <nitegeist.dev@gmail.com>
2023-04-25 20:12:25 -0500

    Add chat login page styles

--- src/components/chat/login.tsx
+++ src/components/chat/login.tsx
@@ -1,24 +1,30 @@
-import { toast } from 'react-toastify'
-import useWallet from '@/hooks/wallets/useWallet'
-import { loginWithCometChat, signUpWithCometChat } from '../../services/chat'
 import { useCometChat } from '@/hooks/cometChat'
 import useSafeAddress from '@/hooks/useSafeAddress'
+import useWallet from '@/hooks/wallets/useWallet'
+import NewChatIcon from '@/public/images/chat/chat.svg'
+import LogoGreen from '@/public/images/logo-green.svg'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import { Box, Button, Grid, Paper, Stack, SvgIcon, Typography } from '@mui/material'
+import { Container } from '@mui/system'
 import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { loginWithCometChat, signUpWithCometChat } from '../../services/chat'
+import css from './styles.module.css'
 
 //@ts-ignore
 const Login = ({ setCurrentUser, setMessages }) => {
   const wallet = useWallet()
   const safeAddress = useSafeAddress()
-  const chat = useCometChat(safeAddress, setMessages);
+  const chat = useCometChat(safeAddress, setMessages)
 
   useEffect(() => {
     if (!chat || !wallet?.address) return
-    handleLogin();
+    handleLogin()
   }, [wallet, safeAddress, chat])
 
   const handleLogin = async () => {
     console.log(wallet)
-    if (!wallet?.address) return;
+    if (!wallet?.address) return
     await toast.promise(
       new Promise(async (resolve, reject) => {
         await loginWithCometChat(wallet?.address)
@@ -32,8 +38,8 @@ const Login = ({ setCurrentUser, setMessages }) => {
           })
       }),
       {
-        pending: 'Signing in...',
-        success: 'Logged in successful 👌',
+        pending: 'Logging in...',
+        success: 'Logged in successfully 👌',
         error: 'Error, are you signed up? 🤯',
       },
     )
@@ -54,17 +60,63 @@ const Login = ({ setCurrentUser, setMessages }) => {
       }),
       {
         pending: 'Signing up...',
-        success: 'Signned up successful 👌',
+        success: 'Signed up successfully 👌',
         error: 'Error, maybe you should login instead? 🤯',
       },
     )
   }
 
   return (
-    <div className="flex justify-start items-center space-x-2">
-      <button onClick={handleLogin}>Login Now</button>
-      <button onClick={handleSignup}>Signup Now</button>
-    </div>
+    <Container fixed sx={{ height: '100vh', width: '100vw' }}>
+      <Box
+        sx={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid item xs lg={4} order={{ xs: 2, sm: 1 }}>
+            <Paper sx={{ padding: 4, height: 1, maxWidth: '400px' }} className={css.loginwindowelement}>
+              <SvgIcon component={NewChatIcon} inheritViewBox sx={{ width: '42px', height: '42px' }} />
+              <Typography variant="h3" fontWeight={700} mb={1} mt={3}>
+                Chat with your safe members now
+              </Typography>
+
+              <Typography variant="body2" mb={3}>
+                Online messaging and signing transactions made easy.
+              </Typography>
+
+              <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2}>
+                <Button variant="contained" onClick={handleLogin}>
+                  Log in
+                </Button>
+                <Button variant="text" onClick={handleSignup}>
+                  Sign up <ChevronRightIcon />
+                </Button>
+              </Stack>
+            </Paper>
+          </Grid>
+          <Grid item xs order={{ xs: 1, sm: 2 }}>
+            <Box
+              sx={{
+                height: '100%',
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <SvgIcon component={LogoGreen} inheritViewBox sx={{ width: '300px', height: '300px' }} />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   )
 }
 
